5710075b256ecab685a847a4bab993b93bff01a1
dimonlev <dimon_lev@mail.ru>
2020-10-25 23:19:09 +0300

    init: brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let newStr = str;
+
+  for (const item of bracketsConfig) {
+    arr.push(item[0] + item[1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      newStr = newStr.replace(arr[j], '');
+    }
+    if (newStr.length == 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
